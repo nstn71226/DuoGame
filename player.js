65650425e663abd729716be9b3e42511f472fc14
Nguyensytuan <hocnhudien248@gmail.com>
2026-05-13 02:39:46 +0700

    update game

--- player.js
+++ player.js
@@ -382,7 +382,7 @@ export class Player {
     
     if (this.mixer && !this.currentAction && this.animations.length > 0) this.playAnimation('Idle');
 
-    const speed = 0.2; 
+    const speed = 0.06; 
     let isMoving = false; 
 
     if (this.isLocal) {
